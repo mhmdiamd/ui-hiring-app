74e6401228de78ebd3f296503a1e26a466f833e0
mhmdiamd <darmawanilham34@gmail.com>
2023-02-25 15:35:59 +0700

    ci: add Profile page and left side card profile for all role user

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
 import '@/styles/globals.css'
+import '@/styles/navbarStyle.css'
+import '@/styles/style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect } from 'react'
+import { Provider } from 'react-redux'
+import { store } from '@/app/store'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.min.js')
+  }, [])
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
