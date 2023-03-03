742ba6b57bdeb5f63c78d193211b46a218d70a8e
mhmdiamd <darmawanilham34@gmail.com>
2023-03-03 16:01:16 +0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   env: {
     API_ENDPOINT: 'http://localhost:3001/api/v1',
   }
